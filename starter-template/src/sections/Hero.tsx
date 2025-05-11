f677b6d70d497aca0e7c5a0576b6b758162a4126
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-11 07:47:16 +0530

    done with the hero section

--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -1,14 +1,29 @@
 import Button from "@/components/Button";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
+import Pointer from "@/components/Pointer";
+import Image from "next/image";
 
 export default function Hero() {
-    return <section className="py-24">
-        <div className="container">
-
+    return <section className="py-24 overflow-x-clip">
+        <div className="container relative">
+            <div className="absolute -left-32 top-16 hidden lg:block">
+                <Image src={designExample1Image} alt="design example 1 image"/>
+            </div>
+            <div className="absolute -right-64 -top-16 hidden lg:block">
+                <Image src={designExample2Image} alt="design example 2 image"/>
+            </div>
+            <div className="absolute left-56 top-96 hidden lg:block">
+                <Pointer name="Neha" />
+            </div>
+            <div className="absolute right-80 -top-4 hidden lg:block">
+                <Pointer name="Subh" color="red" />
+            </div>
             <div className="flex justify-center">
             <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">✨ $10.5M seed round raised </div>
             </div>
             
-            <h1 className="text-6xl font-medium text-center mt-6">Impactful design , created effortlessly</h1>
+            <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">Impactful design , created effortlessly</h1>
             <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">Design tools shouldn&apos;t slow you down. Layers combines powerful features with an intuitive interface that keeps you in your creative flow.</p>
         </div>
         <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto ">
