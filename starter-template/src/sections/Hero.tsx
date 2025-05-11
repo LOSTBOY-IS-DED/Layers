b3daa93dc0bf9b1ae9c0eeb5b3374bac8d5e97b9
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-11 06:14:15 +0530

    hero section for tab view done

--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -1,13 +1,19 @@
+import Button from "@/components/Button";
+
 export default function Hero() {
-    return <section>
+    return <section className="py-24">
         <div className="container">
-            <div>$10.5M seed round raised </div>
-            <h1>Impactful design , created effortlessly</h1>
-            <p>Design tools shouldn&apos;t slow you down. Layers combines powerful features with an intuitive interface that keeps you in your creative flow.</p>
+
+            <div className="flex justify-center">
+            <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">✨ $10.5M seed round raised </div>
+            </div>
+            
+            <h1 className="text-6xl font-medium text-center mt-6">Impactful design , created effortlessly</h1>
+            <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">Design tools shouldn&apos;t slow you down. Layers combines powerful features with an intuitive interface that keeps you in your creative flow.</p>
         </div>
-        <form>
-            <input type="email" placeholder="Enter you email " />
-            <button type="submit">Sign Up </button>
+        <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto ">
+            <input type="email" placeholder="Enter you email " className="bg-transparent px-4 flex-1" />
+            <Button variant="primary" type="submit" className="whitespace-nowrap" size="sm">Sign Up </Button>
         </form>
     </section>;
-}
+} 
