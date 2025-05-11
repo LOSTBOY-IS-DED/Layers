06509ca4b69e6b8324c251fc8bd8e3cfc4dbd7c8
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-11 05:45:42 +0530

    added contents of hero component now the designing part

--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -1,3 +1,13 @@
 export default function Hero() {
-    return <div>Hero</div>;
+    return <section>
+        <div className="container">
+            <div>$10.5M seed round raised </div>
+            <h1>Impactful design , created effortlessly</h1>
+            <p>Design tools shouldn&apos;t slow you down. Layers combines powerful features with an intuitive interface that keeps you in your creative flow.</p>
+        </div>
+        <form>
+            <input type="email" placeholder="Enter you email " />
+            <button type="submit">Sign Up </button>
+        </form>
+    </section>;
 }
