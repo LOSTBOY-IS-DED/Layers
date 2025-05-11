a535a0c1aef31f8d9b8265873e27f9c013230cac
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-11 15:22:23 +0530

    done with intro section mobile view

--- starter-template/src/sections/Introduction.tsx
+++ starter-template/src/sections/Introduction.tsx
@@ -1,5 +1,18 @@
 const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
+import Tag from "@/components/Tag";
+
 
 export default function Introduction() {
-    return <div>Introduction</div>;
+    return <section className="py-28">
+        <div className="container">
+            <div className="flex justify-center">
+            <Tag>Introducing Layers</Tag>
+            </div>
+            <div className="text-4xl font-medium text-center mt-10">
+                <span>Your Creative process deserves better.</span>{" "}
+                <span className="text-white/15">{text}</span>
+                <span className="text-lime-400 block">That&apos;s why we built Layers</span>
+            </div>
+        </div>
+    </section>;
 }
