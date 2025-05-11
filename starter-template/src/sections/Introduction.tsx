8916c84a34e3cb1510168278a2ecf745e9b468cf
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-11 15:24:37 +0530

    done with the tablet and desktop view of intro section

--- starter-template/src/sections/Introduction.tsx
+++ starter-template/src/sections/Introduction.tsx
@@ -3,15 +3,15 @@ import Tag from "@/components/Tag";
 
 
 export default function Introduction() {
-    return <section className="py-28">
+    return <section className="py-28 lg:py-40">
         <div className="container">
             <div className="flex justify-center">
             <Tag>Introducing Layers</Tag>
             </div>
-            <div className="text-4xl font-medium text-center mt-10">
+            <div className="text-4xl md:text-6xl lg:text-7xl font-medium text-center mt-10">
                 <span>Your Creative process deserves better.</span>{" "}
                 <span className="text-white/15">{text}</span>
-                <span className="text-lime-400 block">That&apos;s why we built Layers</span>
+                <span className="text-lime-400 block">That&apos;s why we built Layers.</span>
             </div>
         </div>
     </section>;
