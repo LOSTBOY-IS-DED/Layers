660d831c4a49d768cda4fd84ad2aff2caf28ad37
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-12 12:19:10 +0530

    integration seciton mobile view without design

--- starter-template/src/sections/Integrations.tsx
+++ starter-template/src/sections/Integrations.tsx
@@ -1,36 +1,64 @@
+import Tag from "@/components/Tag";
+import figmaIcon from "@/assets/images/figma-logo.svg";
+import notionIcon from "@/assets/images/notion-logo.svg";
+import slackIcon from "@/assets/images/slack-logo.svg";
+import relumeIcon from "@/assets/images/relume-logo.svg";
+import framerIcon from "@/assets/images/framer-logo.svg";
+import githubIcon from "@/assets/images/github-logo.svg";
+import { ImageResponse } from "next/server";
+import Image from "next/image";
+
+
 const integrations = [
     {
         name: "Figma",
-        icon: "figma-logo.svg",
+        icon: figmaIcon,
         description: "Figma is a collaborative interface design tool.",
     },
     {
         name: "Notion",
-        icon: "notion-logo.svg",
+        icon: notionIcon,
         description: "Notion is an all-in-one workspace for notes and docs.",
     },
     {
         name: "Slack",
-        icon: "slack-logo.svg",
+        icon: slackIcon,
         description: "Slack is a powerful team communication platform.",
     },
     {
         name: "Relume",
-        icon: "relume-logo.svg",
+        icon: relumeIcon,
         description: "Relume is a no-code website builder and design system.",
     },
     {
         name: "Framer",
-        icon: "framer-logo.svg",
+        icon: framerIcon,
         description: "Framer is a professional website prototyping tool.",
     },
     {
         name: "GitHub",
-        icon: "github-logo.svg",
+        icon: githubIcon,
         description: "GitHub is the leading platform for code collaboration.",
     },
 ];
 
 export default function Integrations() {
-    return <div>Integrations</div>;
+    return <section>
+        <div className="container">
+            <Tag>Integrations</Tag>
+            <h2>Plays well with <span>others</span></h2>
+            <p>Layers seamlessly connects with your favorite tool, making it easy to plug into any workflow and collaborate across platform </p>
+            <div>
+                {integrations.map(integration => (
+                    <div key={integration.name}>
+                        <div>
+                            <Image src={integration.icon} alt={integration.name} />
+                        </div>
+                        <h3>{integration.name}</h3>
+                        <p>{integration.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </section>;
 }
